8b5c4ac5277568a505cd3b833f0197e70baedf9e
Carolina <96888706+Carowa27@users.noreply.github.com>
2024-01-09 11:04:25 +0100

    (ref) add dot loading active

--- FrontEnd/src/components/LoadingModule.tsx
+++ FrontEnd/src/components/LoadingModule.tsx
@@ -1,11 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LanguageContext } from "../globals/language/language";
 
 export const LoadingModule = () => {
   const { language } = useContext(LanguageContext);
+  const [isThirdDot, setThirdDot] = useState<boolean>(false);
+  useEffect(() => {
+    setInterval(() => {
+      setThirdDot(true);
+    }, 500);
+    setInterval(() => {
+      setThirdDot(false);
+    }, 1000);
+  }, []);
   return (
     <>
-      <h5>{language.lang_code.word_loading}...</h5>
+      <h5>
+        {language.lang_code.word_loading}..
+        {isThirdDot ? <>.</> : null}
+      </h5>
     </>
   );
 };
