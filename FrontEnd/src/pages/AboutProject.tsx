22b67f5617d96e478beb8fe7f57b3a503295bdd3
Carolina <96888706+Carowa27@users.noreply.github.com>
2024-01-10 11:30:45 +0100

    (ref) remove and update frontPageBtnCard

--- FrontEnd/src/pages/AboutProject.tsx
+++ FrontEnd/src/pages/AboutProject.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { LanguageContext } from "../globals/language/language";
-import { FrontPageBtnCard } from "../components/FrontPageBtnCard";
 import { ThemeContext } from "../globals/theme";
 import { useMediaQuery } from "react-responsive";
 import { variables } from "../globals/variables";
@@ -20,22 +19,82 @@ export const AboutProject = () => {
         id="about-container"
         className={isDesktop ? "row my-1" : "column my-1"}
       >
-        <FrontPageBtnCard bkcolor={`${theme.primaryColors.background.hex}`}>
+        <div
+          className={
+            isDesktop
+              ? "rounded px-3 py-2 my-2 d-flex flex-column"
+              : "w-100 rounded px-4 py-3 my-2 d-flex flex-column order-3"
+          }
+          style={
+            isDesktop
+              ? {
+                  width: "40%",
+                  border: `2px solid rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                  minHeight: "85vh",
+                  height: "auto",
+                }
+              : {
+                  border: `2px solid  rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                }
+          }
+        >
           <h5 id="about-card-examprj-header">
             {language.lang_code.about_exam}
           </h5>
           <p>{language.lang_code.about_description_exam}</p>
-        </FrontPageBtnCard>
-        <FrontPageBtnCard bkcolor={`${theme.primaryColors.background.hex}`}>
+        </div>
+        <div
+          className={
+            isDesktop
+              ? "rounded px-3 py-2 my-2 d-flex flex-column"
+              : "w-100 rounded px-4 py-3 my-2 d-flex flex-column order-3"
+          }
+          style={
+            isDesktop
+              ? {
+                  width: "40%",
+                  border: `2px solid rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                  minHeight: "85vh",
+                  height: "auto",
+                }
+              : {
+                  border: `2px solid  rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                }
+          }
+        >
           <h5 id="about-card-purpose-header">
             {language.lang_code.word_purpose}
           </h5>
           <p>{language.lang_code.about_description_purpose}</p>
-        </FrontPageBtnCard>
-        <FrontPageBtnCard bkcolor={`${theme.primaryColors.background.hex}`}>
+        </div>
+        <div
+          className={
+            isDesktop
+              ? "rounded px-3 py-2 my-2 d-flex flex-column"
+              : "w-100 rounded px-4 py-3 my-2 d-flex flex-column order-3"
+          }
+          style={
+            isDesktop
+              ? {
+                  width: "40%",
+                  border: `2px solid rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                  minHeight: "85vh",
+                  height: "auto",
+                }
+              : {
+                  border: `2px solid  rgba(${theme.typeColors.metal.rgb},0.5)`,
+                  backgroundColor: `rgba(${theme.typeColors.metal.rgb},0.1)`,
+                }
+          }
+        >
           <h5 id="about-card-goal-header">{language.lang_code.word_goal}</h5>
           <p className="m-0">{language.lang_code.about_description_goal}</p>
-        </FrontPageBtnCard>
+        </div>
       </div>
     </>
   );
