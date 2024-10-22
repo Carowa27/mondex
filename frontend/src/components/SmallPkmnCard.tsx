c249dfcf8bb6242b7fe9cb69bc02d06a5cd063b0
Carolina <carolina.warntorp@outlook.com>
2024-10-22 11:41:59 +0200

    (bug) fix pkmncard width on mobile

--- frontend/src/components/SmallPkmnCard.tsx
+++ frontend/src/components/SmallPkmnCard.tsx
@@ -165,7 +165,7 @@ export const SmallPkmnCard = ({
         itemRef.current?.clientWidth
           ? itemRef.current?.clientWidth
           : window.innerWidth && !isDesktop && !isTablet
-          ? window.innerWidth / 2 - 30
+          ? window.innerWidth / 3 + 30
           : 180
       );
     }
