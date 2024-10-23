555a4ff88c4413fdf9b9666663a66ce81677d731
Carolina <carolina.warntorp@outlook.com>
2024-10-23 12:02:53 +0200

    (ref) update about

--- frontend/src/pages/AboutProject.tsx
+++ frontend/src/pages/AboutProject.tsx
@@ -47,9 +47,55 @@ export const AboutProject = () => {
           }
         >
           <h5 id="about-card-examprj-header">
-            {language?.lang_code.about_exam}
+            {language?.name === "English" ? (
+              <>History and goals</>
+            ) : (
+              <>Historia och mål</>
+            )}
           </h5>
-          <p className="m-0">{language?.lang_code.about_description_exam}</p>
+          <p className="m-0">
+            {language?.name === "English" ? (
+              <>
+                Originally, this is my graduation project from 2023 when I
+                studied to be a Frontend developer.
+                <br /> <br /> Our task was to create an optional project that
+                reflects our knowledge from the education. I chose to create
+                Mondex, an application where the user can search for pokemon
+                cards from the TCG series. Search for the sets, cards, artists
+                and more. Create collections to see which cards you as a user
+                own and have left in your set or even create a collection with
+                your favorite Pokémon. <br /> <br /> Right now, all data saved
+                in your browser is collected, but you can delete the saved data
+                at any time via your pages. You can also export your data to a
+                csv file. <br /> <br /> In the future, I hope to further develop
+                this app so that it has a proper login, with data saved in the
+                database and opens up opportunities to see other users and even
+                be able to match with cards you want to exchange.
+              </>
+            ) : (
+              <>
+                Ursprungligen är detta mitt examens projekt från 2023 när jag
+                studerade till Frontend utvecklare.
+                <br /> <br />
+                Vår uppgift varatt skapa ett valfritt projekt som återspeglar
+                våra kunskaper från utbildningen. Jag valde att skapa Mondex, en
+                applikation där användaren kan söka efter pokémonkort från
+                TCG-serien. Söka efter uppsättningarna, korten, artisterna och
+                mer. Skapa samlingar för att se vilka kort du som användare äger
+                och har kvar i set eller till och med skapa en samling med din
+                favorit Pokémon.
+                <br /> <br />
+                Just nu samlas all data som sparas i din browser, men du kan
+                närsomhelst ta bort den sparade datan via dina sidor. Du kan
+                även exportera din data till en csv fil.
+                <br />
+                <br />I framtiden hoppas jag att vidareutveckla denna app så att
+                den har en ordentlig inloggning, med data sparad i databas och
+                öppna upp för möjligheter att kunna se andra användare och
+                tillochmed kunna matcha med kort man vill byta.
+              </>
+            )}
+          </p>
         </div>
         <div
           className={
