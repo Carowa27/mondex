fa87f0f472ee28dbaa91506aa4fbdce89c130d2f
Carolina <carolina.warntorp@outlook.com>
2024-10-27 15:30:00 +0100

    (bug) fix build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,14 +3,22 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
   build: {
     // generate .vite/manifest.json in outDir
-    manifest: true,
+    // manifest: true,
+    outDir: "dist",
     rollupOptions: {
       // overwrite default .html entry
       // input: "/path/to/main.tsx",
-      input: "src/main.tsx",
+      // input: { main: "src/main.tsx" },
+      // output: {
+      //   manualChunks: {
+      //     vendor: ["react", "react-dom"],
+      //     // Add more custom chunks as needed
+      //   },
+      //   //   minifyInternalExports: true,
+      // },
     },
   },
 });
